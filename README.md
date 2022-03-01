5d3432fe6bdb3d42144ff0cd6f515e171591259b
JaG <j13ag0@gmail.com>
2022-03-01 17:34:55 -0500

    committing initial code

---
+++ README.md
@@ -0,0 +1,9 @@
+# parcel-GLTFloader-test
+
+Shows a problem with loading a .glb file using GLTFloader when bundled with Parcel 2.
+
+### Install
+`npm install`
+
+### Start
+`npm start`
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+function run() {
+    loadData();
+    //  main loop
+}
+
+run();
+
+
+function loadData() {
+    let gltfLoader = new GLTFLoader();
+
+    let url = new URL( "../assets/models/cubeFish.glb", 
+                   import.meta.url );
+    let str = "" + url;
+console.log( "url:  " + str );
+
+    gltfLoader.load( str, gltfFishReader, null, null );
+}
+
+
+function gltfFishReader( gltf ) {
+    let fishModel = null;
+
+    fishModel = gltf.scene;
+
+    if( fishModel != null ) {
+        console.log("fish loaded:  " + fishModel );
+    }
+}
