aa7d7577535ed48abead2580b6c0909167775bf0
JaG <j13ag0@gmail.com>
2022-03-08 15:26:44 -0500

    set three back to r129

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as THREE from 'three';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 function run() {
     loadData();
